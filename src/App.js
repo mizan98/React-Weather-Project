7dffe65f4317d6fa0df82c9f01900cadc2aaf59e
Mizan Choudhury <mizan_98@hotmail.co.uk>
2019-11-30 17:43:54 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
+import Weather from "./Components/Weather";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//---- API KEY ----//
+const apiKey = "8ddceeacaf8b95fe943c88fc8389dee0"
+
+const Title = () => {
+  return(
+
+    //----- Basic text information on screen ----//
+    <div>
+      <h1 className="title-cpntainer__title">Weather Finder</h1>
+      <h3 className="title-container__subtitle">
+        Find out temperature, conditions and more
+      </h3>
     </div>
-  );
+  )
 }
 
+//---- Form for entering location info ----//
+const Form = ({onWeather}) => {
+  return(
+    <form onSubmit={e => onWeather(e)}>
+    <input type="text" name="city" placeholder="City..."/>
+    <input type="text" name="country" placeholder="Country..."/>
+    <button className="form-button">get Weather</button>
+    </form>
+  )
+}
+
+//---- Getting the weather information ----//
+class App extends Comment {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async e => {
+    e.preventDefault()
+    const city = e.currentTarget.elements.city.value;
+    const country = e.currentTarget.elements.country.value;
+
+    //---- making sure correct information is displayed ----//
+    if (city && country) {
+      try{
+        const apiCall = fetch(
+          `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+        )
+        const {main, sys, name, weather} = await apiCall.json()
+        this.setState({
+          temperature: main.temperature,
+          city: name,
+          country: sys.country,
+          humidity: main.humidity,
+          description: weather[0].description,
+          error: ""
+        })
+      } catch(ex) {
+        console.log(ex.message)
+      }
+      } else {
+        this.setState ({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "please enter a valid place"
+      })
+    }
+  }
+
+  //---- Displaying Weather information on webpage ----//
+  render(){
+    return(
+      <div className="wrapper">
+        <div className="main">
+          <div className="container" style={{width: "100%"}}>
+            <div className="row">
+              <div className="col-xs-5 form-container">
+                <Title />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form onWeather={this.getWeather} />
+                <Weather
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
